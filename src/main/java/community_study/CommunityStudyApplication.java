f769b70babc8ade61627dc851b941d80730a79be
unknown <18483653203@163.com>
2020-09-12 16:43:01 +0800

    init repo

---
+++ src/main/java/community_study/CommunityStudyApplication.java
@@ -0,0 +1,13 @@
+package community_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityStudyApplication.class, args);
+    }
+
+}
