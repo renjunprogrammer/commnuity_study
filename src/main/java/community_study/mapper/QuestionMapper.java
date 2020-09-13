7801805f089820148706aebce09fc3700d457bc0
unknown <18483653203@163.com>
2020-09-13 22:34:38 +0800

    add publish question function

---
+++ src/main/java/community_study/mapper/QuestionMapper.java
@@ -0,0 +1,13 @@
+package community_study.mapper;
+
+import community_study.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface QuestionMapper {
+    @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag) values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void createQuestion(Question question);
+}
